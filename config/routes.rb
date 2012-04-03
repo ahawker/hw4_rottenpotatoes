843e62c762e85ebb15135b26741ed19bc4d443eb
Ubuntu <ubuntu@ip-10-252-77-82.us-west-2.compute.internal>
2012-04-03 06:30:23 +0000

    Bunch of fixes. Still crashing with weird view error though.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rottenpotatoes::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :movies
+  match ':movies/:id' => 'movies#same_director', :as => :same_director_path 
 
   # Sample resource route with options:
   #   resources :products do
